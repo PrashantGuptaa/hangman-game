ab4ff060e40bd096b2a37a9588d9d3c9eaf43679
PrashantGuptaa <meengineerprashant@gmail.com>
2022-12-08 00:00:18 +0530

    hangman - restart functionality

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ const wordCollection = [
   "Hardware",
 ];
 
-let selectedWord = null;
+let selectedWord = null,
+  livesLeft = 5;
+
+  const ENABLED = "enabled", DISABLED = "disabled";
 
 (function () {
   const keyboardContainerElem = document.getElementById("keyboard-container");
@@ -26,23 +29,63 @@ let selectedWord = null;
     keyboardContainerElem.appendChild(btnElem);
   }
   selectWordRandomly();
+  document.getElementById("lives").innerHTML = livesLeft;
 })();
 
 function selectWordRandomly() {
-    const index = Math.round(Math.random() * 10);
-    selectedWord = wordCollection[index];
-    const wordContainerElem = document.getElementById("word-container");
+  const index = Math.round(Math.random() * 10);
+  selectedWord = wordCollection[index];
+  const wordContainerElem = document.getElementById("word-container");
 
-    for (let i = 0 ; i < selectedWord.length; i++) {
-        const boxElem = document.createElement('div');
-        boxElem.setAttribute("class", "box");
-        boxElem.setAttribute("id", i);
-        wordContainerElem.appendChild(boxElem);
-    }
-
+  for (let i = 0; i < selectedWord.length; i++) {
+    const boxElem = document.createElement("div");
+    boxElem.setAttribute("class", "box complete-center");
+    boxElem.setAttribute("id", i);
+    wordContainerElem.appendChild(boxElem);
+  }
 }
 
 function handleClick(e) {
-  console.log("Handling Click", e.target.innerHTML);
+  const clickedLetter = e.target.innerHTML;
+  let letterExists = false;
+  for (let i = 0; i < selectedWord.length; i++) {
+    if (clickedLetter.toLowerCase() === selectedWord[i].toLowerCase()) {
+      document.getElementById(i).innerHTML = selectedWord[i];
+      letterExists = true;
+    }
+  }
+  if (!letterExists) {
+    document.getElementById("lives").innerHTML = --livesLeft;
+  }
+  if(livesLeft <= 0) {
+    updateCompleteKeyboard(true);
+  }
+  updateKey(e.target.id, true);
+}
+
+function updateCompleteKeyboard(operation) {
+  for (i = 65; i < 91; i++) {
+    const letter = String.fromCharCode(i);
+    updateKey(letter, operation);
+  }
+}
+
+function updateKey(id, operation) {
+  document.getElementById(id).disabled = operation;
+}
+
+function handleRestart() {
+    updateCompleteKeyboard(false);
+    livesLeft = 5;
+    document.getElementById('lives').innerHTML = livesLeft;
+    removeElements();
+    selectWordRandomly();
+}
+function removeElements() {
+    const wordContainerElem = document.getElementById("word-container");
+    const boxesElem = document.getElementsByClassName('box'); // return []
+    while (boxesElem.length > 0) {
+        wordContainerElem.removeChild(boxesElem[boxesElem.length - 1]);
+    }
 }
 
